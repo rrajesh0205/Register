a58b4907718a6f5e6a7bd576e763453de267157a
rrajesh0205 <61044061+rrajesh0205@users.noreply.github.com>
2020-04-27 23:26:29 +0530

    27042020at1125PMIST

--- insta_project/posts/models.py
+++ insta_project/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 100)
-    cover = models.ImageField(upload_to='images/')
+    cover = models.ImageField(upload_to='images/', blank=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
