967a1acfa6ebda6998badd7d259f8745ce066b4e
rrajesh0205 <61044061+rrajesh0205@users.noreply.github.com>
2020-09-29 23:08:56 +0530

    Edit and Delete Post is now available only in Icons. It has been aligned right now. Dated 29092020 1108PM IST

--- student/studentapp/views.py
+++ student/studentapp/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, CreateView
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy  
 from .forms import StudentForm
 from .models import Student 
@@ -11,6 +12,7 @@ def list_and_create(request):
     form = StudentForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
         form.save()
+        return HttpResponseRedirect('/')
 
     # notice this comes after saving the form to pick up new objects
     objects = Student.objects.all()
